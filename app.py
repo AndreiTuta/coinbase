b937e058c42fe04afea9509a132fb4f1291ddf07
andreituta <tuta.andrei96@gmail.com>
2020-06-28 15:22:25 +0100

    Coinbase-004:

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # imports
+import sys
+
 import json
 import urllib
 import configparser
+import datetime
 import time
 #coinbase classes
 from coinbase.wallet.client import Client
@@ -10,23 +13,64 @@ from pprint import pprint
 # custom classes
 from wallet import Wallet
 
-def coinbase_call():
-    # load config from file
-    config = configparser.RawConfigParser()
-    config.read('config.ini')
-    details_dict = dict(config.items('coinbase'))
+# TODO: move to separate file
+# GLOBAL config
+# load config from file
+config = configparser.RawConfigParser()
+config.read('config.ini')
+details_dict = dict(config.items('coinbase'))
+
+timer = 1
+mode = 'debug'
+
+
+
+def init_client():
     # get api_key and secret
     api_key = details_dict['api_key']
     api_secret = details_dict['api_secret']
     # create a client
     client = Client(api_key, api_secret)
     # get all accounts
+    return client
+
+def coinbase_call(client):
     return client.get_account(details_dict['account_id'])
 
-cb_wallet = coinbase_call()
-wallet = Wallet(cb_wallet)
-# TODO: add current time
-# csv print
-print(wallet.print_to_csv())
-# pretty print
-#pprint(wallet.print())
+def coinbase_buy_price(client, crypto, currency):
+    return client.get_buy_price(currency_pair = crypto+'-'+currency)["amount"]
+
+def coinbase_sell_price(client, crypto, currency):
+    return client.get_sell_price(currency_pair = crypto+'-'+currency)["amount"]
+
+def check_balance(client):
+    cb_wallet = coinbase_call(client)
+    wallet = Wallet(cb_wallet)
+    return wallet.get_sequence()
+
+def display_stats(current):
+    client = init_client()
+    seq = check_balance(client)
+    # add time
+    seq.insert(0, str(current.hour) + ":" + str(current.minute))
+    # add buy price
+    seq.insert(len(seq) + 1, str(coinbase_buy_price(client, seq[3], seq[5])))
+    # add sell price
+    seq.insert(len(seq) + 1, str(coinbase_sell_price(client, seq[3], seq[5])))
+    print(', '.join(seq))
+
+def handle_arg(argv):
+    timer = argv[0]
+    mode = argv[1]
+    print(timer)
+    print(mode)
+
+def main(argv):
+    handle_arg(argv)
+    while True:
+        current = datetime.datetime.now()
+        display_stats(current)
+        time.sleep(1 *60)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
