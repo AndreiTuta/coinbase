4d534f0dd44bac841124cc94e5016509e1c52c1b
andreituta <tuta.andrei96@gmail.com>
2020-07-05 12:24:48 +0100

    Coinbase-011: Update to client and improve waller

--- cb_client.py
+++ cb_client.py
@@ -1,17 +1,29 @@
-#coinbase classes
+# coinbase classes
 from coinbase.wallet.client import Client
 # custom classes
-import wallet
+from wallet import Wallet
+
 
 class CoinBaseClient:
-    def __init__(self,details_dict):
+    def __init__(self, details_dict):
         # create a client
-        self.client = Client(details_dict['api_key'], details_dict['api_secret'])
-        # get all accounts
-    
+        self.client = Client(
+            details_dict['api_key'], details_dict['api_secret'])
+        self.fetch_accounts()
+        
+    # internal use
     def get_client_accounts(self):
         return self.client.get_accounts()
-    
-    def get_client_wallets(self):
-        accounts = self.client.get_accounts()['data']
-        return accounts
+
+    def fetch_accounts(self):
+        self.wallets = list()
+        accounts = self.get_client_accounts()['data']
+        for account in accounts:
+            account_wallet = Wallet(account).toStr()
+            self.wallets.append(account_wallet)
+            
+    # external use
+    def get_client_wallets(self, refresh):
+        if(refresh):
+            self.fetch_accounts()
+        return self.wallets
--- wallet.py
+++ wallet.py
@@ -1,24 +1,7 @@
 class Wallet:
     # TODO: make all columns constats
-    def __init__(self,cb_wallet):
+    def __init__(self, cb_wallet):
         self.__dict__.update(cb_wallet)
-        self.__dict__.pop('balance', None)
-        self.__dict__.pop('native_balance', None)
-        for k, v in cb_wallet.items():
-            if isinstance(v, dict):
-                val ={}
-                for k,v in v.items():
-                    val[k] = v
-                if(val['currency'] =='ETH'):
-                    self.__dict__['balance'] = val['amount']
-                else:
-                    self.__dict__['native_balance'] = val['amount']
-                    self.__dict__['native_balance_currency'] = val['currency']
 
-
-    def get_sequence(self):
-        sequence = [self.__dict__['balance'], self.__dict__['currency'], self.__dict__['native_balance'], self.__dict__['native_balance_currency']]
-        return sequence
-
-    def print(self):
+    def toStr(self):
         return vars(self)
